3475010d1392a934d569022980975fa244f43a70
sayujkuickel <sayujkuickel@gmail.com>
2025-06-20 23:26:32 +0545

    Add sitemap for google indexing

---
+++ generate-sitemap.cjs
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
+const baseUrl = "https://bus-routes.sayuj.com.np";
+const staticRoutes = [
+  "",
+  "routes",
+  "stops",
+  "search",
+  "contact",
+  "about",
+  "bus",
+];
+
+const routeDataPath = path.join(__dirname, "src", "data", "route_data.json");
+const routeData = JSON.parse(fs.readFileSync(routeDataPath, "utf-8"));
+
+let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+
+staticRoutes.forEach((route) => {
+  xml += `  <url>\n    <loc>${baseUrl}/${route}</loc>\n    <priority>0.8</priority>\n  </url>\n`;
+});
+
+routeData.forEach((route) => {
+  xml += `  <url>\n    <loc>${baseUrl}/bus/${route.id}</loc>\n    <priority>0.6</priority>\n  </url>\n`;
+});
+
+xml += `</urlset>`;
+
+const outputPath = path.join(__dirname, "public", "sitemap.xml");
+fs.writeFileSync(outputPath, xml, "utf-8");
+
+console.log("Generated sitemap!");
