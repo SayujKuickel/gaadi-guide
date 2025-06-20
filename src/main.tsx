1485a068f4853c2ce996fb22a227024f9c51fe9a
sayujkuickel <sayujkuickel@gmail.com>
2025-06-20 23:44:08 +0545

    fixed build issue

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ToastProvider } from "./context/ToastContext.tsx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <>
