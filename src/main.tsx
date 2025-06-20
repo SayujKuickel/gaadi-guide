0c4511fbf073f72394b49d6585acce071952c81c
sayujkuickel <sayujkuickel@gmail.com>
2025-06-20 23:37:04 +0545

    Vercel Analytics integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import "./index.css";
+import App from "./App.tsx";
 import { createRoot } from "react-dom/client";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import { BrowserRouter } from "react-router-dom";
 import { ToastProvider } from "./context/ToastContext.tsx";
-
-import App from "./App.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <>
@@ -13,6 +13,8 @@ createRoot(document.getElementById("root")!).render(
         <App />
       </ToastProvider>
     </BrowserRouter>
+
     <SpeedInsights />
+    <Analytics />
   </>
 );
